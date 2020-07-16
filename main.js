576b8884b0709a711f5667005ba6c7565c12ff59
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-16 14:41:16 -0400

    second if statement

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const getRows = require('./get-rows.js')
 //step seven:
 const getTotalCasesByDay= require('./get-total-cases-by-day.js')
 
+//step nine :
+const getNewCasesByDay = require('./get-new-cases-by-day.js')
 //step one:
 const fs = require('fs');
 //step two:?
@@ -22,5 +24,7 @@ const gotRows = getRows(file)
 console.log(getTotalCasesByDay(userInput2,gotRows))
  }
 
- //step eight
- if(){}
+ //step eight:
+ if(userInput1 === 'new'){
+     console.log(getNewCasesByDay(userInput2, gotRows))
+ }
