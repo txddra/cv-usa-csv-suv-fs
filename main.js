3ca8c2075421caefe78a0e42fcc9a3199ac6bee0
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-16 14:36:37 -0400

    first if statement

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 //step four:
 const getRows = require('./get-rows.js')
+ 
+//step seven:
+const getTotalCasesByDay= require('./get-total-cases-by-day.js')
 
 //step one:
 const fs = require('fs');
 //step two:?
 const userInput1 = process.argv[2];
 const userInput2 = process.argv[3];
+
 //step three:
 const file = fs.readFileSync('./owid-covid-usa-data.csv',  'utf8' );
 
+//step five:
+const gotRows = getRows(file)
+
+//step six:
+ if(userInput1 === 'total'){
+
+console.log(getTotalCasesByDay(userInput2,gotRows))
+ }
+
+ //step eight
+ if(){}
